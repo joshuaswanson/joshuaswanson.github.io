1b0c218b7dca1da1723d7b2e25cae7dbea1c44b9
joshuaswanson <swansonxc@gmail.com>
2020-04-01 21:03:04 -0700

    commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function runOnScroll() {
-    //if (document.body.scrollTop >= 200) {
-        var scrollAmount = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
-
-    document.getElementById("header-cover").style.top = (500 - 0.5 * scrollAmount) + "px";
-    //}
-}
 window.onscroll = function() { runOnScroll() };
+function runOnScroll() {
+    let scrollAmount = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
+    document.getElementById("header-cover").style.top = (500 - 0.5 * scrollAmount) + "px";
+}
+
+
+function showError() {
+    document.getElementById("resume-container").innerHTML = "I haven't uploaded this yet. Sorry! ";
+}
