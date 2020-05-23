c2eb368726000e4de38852ee272e70222d26d2e7
joshuaswanson <joshuaswanson@users.noreply.github.com>
2020-05-22 17:38:29 -0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.onscroll = function() { runOnScroll() };
+/*window.onscroll = function() { runOnScroll() };
 function runOnScroll() {
     let scrollAmount = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop);
     document.getElementById("header-cover").style.top = (500 - 0.5 * scrollAmount) + "px";
@@ -7,4 +7,4 @@ function runOnScroll() {
 
 function showError() {
     document.getElementById("resume-container").innerHTML = "I haven't uploaded this yet. Sorry! ";
-}
+}*/
